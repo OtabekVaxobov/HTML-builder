6f962ca864e7118e9a918e8766205b2c6af2404b
Otabek_Vaxobov <elot2017@mail.ru>
2022-05-19 22:54:23 +0500

    feat: add solution

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,13 +1,24 @@
-// const x = require('fs').readdir('./03-files-in-folder/secret-folder', (err) =>
-//   console.log(err)
-// );
-// console.log(x);
-
-const fs = require('fs');
-const path = './03-files-in-folder/secret-folder/';
-
-const filesArray = fs
-  .readdirSync(path)
-  .filter((file) => fs.lstatSync(path + file).isFile());
-
-console.log(filesArray);
+let path = require('path');
+let info = (file) => {
+  let dat = [];
+  if (file.isFile()) {
+    require('fs').stat(
+      path.resolve(__dirname, 'secret-folder', file.name),
+      (_e, stats) => {
+        dat.push(file.name.split('.').slice(0, -1).join('.'));
+        dat.push(path.extname(file.name).slice(1));
+        dat.push(Math.round(stats.size / 1024) + 'Kb');
+        console.log(dat.join(' - '));
+      }
+    );
+  }
+};
+require('fs').readdir(
+  path.join(__dirname, 'secret-folder'),
+  { withFileTypes: true },
+  (_e, files) => {
+    files.forEach((item) => {
+      info(item);
+    });
+  }
+);
