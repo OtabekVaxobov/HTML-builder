6cbc8d6fef6f50321daf62c91817af1bac8c2fd4
Otabek_Vaxobov <elot2017@mail.ru>
2022-05-19 14:53:51 +0500

    feat: add async code

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,26 +1,11 @@
-var lineReader = require('readline').createInterface({
-  input: require('fs').createReadStream('01-read-file/text.txt'),
-});
-
-lineReader.on('line', function (err, line) {
-  if (err) {
-    console.error(err);
-    return;
+const main = async () => {
+  const stream = require('fs').createReadStream(
+    '01-read-file/text.txt',
+    'utf-8'
+  );
+  for await (const chunk of stream) {
+    console.log(chunk);
   }
-  console.log('Line from file:', line);
-});
+};
 
-// OR
-// const fs = require('fs');
-
-// let foo = () => {
-//   try {
-//     fs.readFile('01-read-file/text.tt', 'utf8', (_error, data) => {
-//       console.log(data);
-//     });
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-
-// foo();
+main().catch(console.error);
