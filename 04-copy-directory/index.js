644d0e426a59b9aa1aea9be55d36afab1a78948f
Otabek_Vaxobov <elot2017@mail.ru>
2022-05-19 23:51:54 +0500

    feat: solved

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const Path = require('path');
+const FSP = require('fs').promises;
 
+async function copyDir(src, dest) {
+  await FSP.rm(dest, {
+    recursive: true,
+    force: true,
+  });
+  const entries = await FSP.readdir(src, { withFileTypes: true });
+  await FSP.mkdir(dest);
+  for (let entry of entries) {
+    const srcPath = Path.join(src, entry.name);
+    const destPath = Path.join(dest, entry.name);
+    if (entry.isDirectory()) {
+      await copyDir(srcPath, destPath);
+    } else {
+      await FSP.copyFile(srcPath, destPath);
+    }
+  }
+}
+copyDir('./04-copy-directory/files', './04-copy-directory/files-copy');
